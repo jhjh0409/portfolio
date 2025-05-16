8c6b0d0df5c6e629f47de7c57701ba6ff572ee1b
Jing Huan <124416920+jhjh0409@users.noreply.github.com>
2025-05-17 02:46:00 +0800

    fix: any type bug error in Navlink

--- components/ui/Navlink.tsx
+++ components/ui/Navlink.tsx
@@ -3,5 +3,5 @@ import { IconProps } from "@tabler/icons-react";
 export type Navlink = {
   href: string;
   label: string;
-  icon?: React.ReactNode | IconProps | any;
+  icon?: React.ReactNode | IconProps;
 };
